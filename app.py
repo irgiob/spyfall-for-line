111378a15a88167dfbc736352b49d0d20ce2a08d
irgiob <irgiob@gmail.com>
2020-01-21 14:10:12 +0700

    first test commit

--- app.py
+++ app.py
@@ -1,45 +1,57 @@
 # spyfall-for-line / app.py
 
-import os 
-from decouple import config 
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError 
-from linebot.models import MessageEvent, TextMessage, TextSendMessage 
+import os
+from decouple import config
+from game_func import *
+from flask import (
+    Flask, request, abort
+)
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, 
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageAction,
+    FollowEvent, JoinEvent, LeaveEvent
+)
 
 app = Flask(__name__)
-# get LINE_CHANNEL_ACCESS_TOKEN from your environment variable 
-line_bot_api = LineBotApi ( 
-    config ( "LINE_CHANNEL_ACCESS_TOKEN", 
-           default = os.environ.get ( 'LINE_ACCESS_TOKEN')) 
-) 
-# get LINE_CHANNEL_SECRET from your environment variable 
-handler = WebhookHandler (
-    config ("LINE_CHANNEL_SECRET", 
-           default = os.environ.get ('LINE_CHANNEL_SECRET')) 
-) 
 
-@ app.route ("/ callback", methods = ['POST']) 
-def callback (): 
-    signature = request.headers [' X-Line-Signature '] 
+# get LINE_CHANNEL_ACCESS_TOKEN from your environment variable
+line_bot_api = LineBotApi(
+    config("LINE_CHANNEL_ACCESS_TOKEN",
+           default=os.environ.get('LINE_ACCESS_TOKEN'))
+)
+# get LINE_CHANNEL_SECRET from your environment variable
+handler = WebhookHandler(
+    config("LINE_CHANNEL_SECRET",
+           default=os.environ.get('LINE_CHANNEL_SECRET'))
+)
 
-    body = request.get_data (as_text = True) 
-    app.logger.info ("Request body:" + body) 
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
 
-    try: 
-        handler.handle (body, signature ) 
-    except InvalidSignatureError: 
-        abort (400) 
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-    return 'OK' 
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-@ handler.add (MessageEvent, message = TextMessage) 
-def handle_text_message (event): 
-    line_bot_api.reply_message ( 
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage (text = event.message.text) 
-    ) 
+        TextSendMessage(text=event.message.text))
 
-if __name__ == "__main__": 
-    port = int (os.environ.get ('PORT', 5000)) 
-    app.run (host = '0.0.0.0', port = port)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
